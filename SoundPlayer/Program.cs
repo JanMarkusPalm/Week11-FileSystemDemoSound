fe96f64cb0bd3df0bf0fc414807bf14b879ba00c
Opilane TTHK <opilane@tthk.ee>
2021-11-10 12:39:53 +0200

    sound

---
+++ SoundPlayer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+using System.Media;
+
+
+namespace SoundPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                 glassplayer = new SoundPlayer("glass.wav");
+                glassplayer.Load();
+                glassplayer.Play();
+            }
+            ForegroundColor = ConsoleColor.White;
+            WriteLine(@"
+                                              ,           ^'^  _
+                                              )               (_) ^'^
+         _/\_                    .---------. ((        ^'^
+         (('>                    )`'`'`'`'`( ||                 ^'^
+    _    /^|                    /`'`'`'`'`'`\||           ^'^
+    =>--/__|m---               /`'`'`'`'`'`'`\|
+         ^^           ,,,,,,, /`'`'`'`'`'`'`'`\      ,
+                     .-------.`|`````````````|`  .   )
+                    / .^. .^. \|  ,^^, ,^^,  |  / \ ((
+                   /  |_| |_|  \  |__| |__|  | /,-,\||
+        _         /_____________\ |')| |  |  |/ |_| \|
+       (')         |  __   __  |  '==' '=='  /_______\     _
+      (' ')        | /  \ /  \ |   _______   |,^, ,^,|    (')
+       \  \        | |--| |--| |  ((--.--))  ||_| |_||   (' ')
+     _  ^^^ _      | |__| |('| |  ||  |  ||  |,-, ,-,|   /  /
+   ,' ',  ,' ',    |           |  ||  |  ||  ||_| |_||   ^^^
+.,,|RIP|,.|RIP|,.,,'==========='==''=='==''=='=======',,....,,,,.,ldb
+
+");
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine("\n(Press any key to exit)");
+            ReadKey();
+        }
+    }
+}
